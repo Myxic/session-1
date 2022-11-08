93a7a158cfd2bbf0f26b71c3e176fe1ac1cc830b
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-08 12:56:51 +0100

    session-1

--- session-1/Program.cs
+++ session-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace session_1
 {
@@ -6,7 +7,26 @@ namespace session_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter word");
+           var string1 =  Console.ReadLine();
+            Console.WriteLine(string1);
+            if (string1.Length < 5)
+            {
+                
+                } else if(string1.Length >=5) {
+                var num = string1.Length - 4;
+                
+                var hash = " ";
+                for (int i = 0; i < num; i++)
+                {
+                    hash += "#";
+                };
+                var lfour = string1.Remove(0, num);
+                
+                var result = hash + lfour;
+                Console.WriteLine(result);
+            }
+           
         }
     }
 }
