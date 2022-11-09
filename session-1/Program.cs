2c73aa957f6bccb86fb4fe7049ae298549f0406b
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-09 16:07:04 +0100

    Update Program.cs

--- session-1/Program.cs
+++ session-1/Program.cs
@@ -7,26 +7,31 @@ namespace session_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter word");
-           var string1 =  Console.ReadLine();
-            Console.WriteLine(string1);
-            if (string1.Length < 5)
+            Console.Write("Enter words: ");
+            var string1 = Console.ReadLine();
+            int requiedWord = 4;
+            
+
+            if (string1.Length <= requiedWord)
             {
+                Console.WriteLine(string1);
+            }
+            else if (string1.Length > requiedWord)
+            {
+                var num = string1.Length - requiedWord;
+
+                var hash = "";
                 
-                } else if(string1.Length >=5) {
-                var num = string1.Length - 4;
-                
-                var hash = " ";
                 for (int i = 0; i < num; i++)
                 {
                     hash += "#";
                 };
                 var lfour = string1.Remove(0, num);
-                
+
                 var result = hash + lfour;
                 Console.WriteLine(result);
             }
-           
+
         }
     }
 }
